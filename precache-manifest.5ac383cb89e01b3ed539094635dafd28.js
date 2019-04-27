44a9a0d4bd214f8f4994e525bee4cea7d571cf15
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2019-04-26 23:23:08 -0500

    Updates

---
+++ precache-manifest.5ac383cb89e01b3ed539094635dafd28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13eb9ac0024473cf3c511a435ac06244",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "bbeb1f81f5bbf5f58442",
+    "url": "/clicky-game/static/css/main.b553217f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bbeb1f81f5bbf5f58442",
+    "url": "/clicky-game/static/js/main.871cac83.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
